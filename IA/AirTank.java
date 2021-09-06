98cab2c187842f7c0a8bd0d8a9ffaf9d9960bc3c
Asaad Al-Barwani <asaad.barwani@gmail.com>
2021-09-06 10:09:04 +0400

    Initial Commit

---
+++ IA/AirTank.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class AirTank here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class AirTank extends Actor
+{
+    /**
+     * Act - do whatever the AirTank wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
